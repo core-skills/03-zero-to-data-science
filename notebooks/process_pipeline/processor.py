c2b550b1751353fa69c55f09e05fa2d1dcfcd07e
Morgan Williams <morgan.j.williams@hotmail.com>
2020-11-03 13:47:55 +0800

    Notebook - Edit note for function; remove redundant fill value

--- notebooks/process_pipeline/processor.py
+++ notebooks/process_pipeline/processor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from pathlib import Path 
 
-def process_csv_file(filepath, output_folder = Path("../data/processed/"), fill_with=np.nan):
+def process_csv_file(filepath, output_folder = Path("../data/processed/")):
     """
     Process a csv file so it's ready for exploratory data analysis.
     
@@ -12,8 +12,6 @@ def process_csv_file(filepath, output_folder = Path("../data/processed/"), fill_
         Path to the csv file to import.
     output_folder 
         Path to the folder where you want the processed version to reside.
-    fill_with 
-        Value to substitute for zero.
         
     Returns
     --------
